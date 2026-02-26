2b6c6e48ac0dcb96a9d85afe5667aaf0d6ca962e
charishma-reddy11 <charishma1107@gmail.com>
2026-02-26 10:44:55 +0530

    deployment commit

--- she_deserves/settings.py
+++ she_deserves/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yka9k^oo^x^voa&!(7rs2_m@!9l(y&62)-u6to$fb+len6#x*6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -46,6 +46,7 @@ AUTH_USER_MODEL = 'accounts1.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+static_root = os.path.join(BASE_DIR, 'staticfiles')
+media_url = '/media/'
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
 LOGIN_REDIRECT_URL = 'travel:home'
 LOGOUT_REDIRECT_URL = 'travel:home'
 

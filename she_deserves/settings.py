4f7880478ceaae54bfb331e6c474503842cfefbc
charishma-reddy11 <charishma1107@gmail.com>
2026-02-26 11:19:35 +0530

    import os modules

--- she_deserves/settings.py
+++ she_deserves/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,7 +125,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 static_root = os.path.join(BASE_DIR, 'staticfiles')
 media_url = '/media/'
-import os
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 

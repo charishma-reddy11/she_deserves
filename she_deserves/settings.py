66c8dcfb4ef160aa4a1f461fcc4f7cb5cef27a07
charishma-reddy11 <charishma1107@gmail.com>
2026-02-26 13:05:20 +0530

    changes in settings

--- she_deserves/settings.py
+++ she_deserves/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-static_root = os.path.join(BASE_DIR, 'staticfiles')
-media_url = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

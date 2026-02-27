56f39f6a52ce50331db6ba0237138897e69743cf
charishma-reddy11 <charishma1107@gmail.com>
2026-02-27 13:05:23 +0530

    removing media paths

--- she_deserves/settings.py
+++ she_deserves/settings.py
@@ -124,9 +124,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 LOGIN_REDIRECT_URL = 'travel:home'
